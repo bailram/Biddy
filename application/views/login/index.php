59b474c2dc22c43c61a30259cda8731fa64c8726
ikaru19 <syafrizalizal17@gmail.com>
2019-03-17 14:20:01 +0700

    add view to login

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -5,12 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Hello</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
     <script src="main.js"></script>
+    <style type="text/css">
+        body{
+            overflow-x:hidden;
+        }
+
+        .login-input{
+            display: block;
+        }
+
+    </style>
 </head>
 <body>
     <?php
         include_once('login_navbar.php');
+        include_once('login_content.php');
     ?>
     
 </body>
---
+++ application/views/login/login_content.php
@@ -0,0 +1,32 @@
+<div class="row login" style="margin:5%;">
+
+    <div class="col-md-3">
+    </div>
+    <div class="col-md-6">
+        <div class="card">
+            <h5 class="card-header">Login</h5>
+            <div class="card-body">
+                <form>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default"><i class="fas fa-user"></i></span>
+                        </div>
+                        <input type="text" title="username" class="form-control login-input" placeholder="username" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default"><i class="fas fa-key"></i></span>
+                        </div>
+                        <input type="password" title="username" class="form-control  login-input" placeholder="password" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                  
+                   
+                    <button type="submit" class="btn btn-primary">Login</button>
+                    <a class="forgot" href="<?php echo site_url('/') ?>"style="float:right;">Register here</a>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+    </div>
+</div> 
