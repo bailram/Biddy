cb86b3fd7d1868395e37bb87eceb608698d302af
bailram <dhimasbayu.9f@gmal.com>
2019-05-08 20:03:54 +0700

    Fix add lelang

--- application/controllers/Lelang.php
+++ application/controllers/Lelang.php
@@ -8,8 +8,8 @@ class Lelang extends CI_Controller
 		$this->load->model('m_home');
 		$this->load->model('m_lelang');
 		if ($this->session->userdata('status') != "login") {
-            redirect(base_url('login'));
-        }
+			redirect(base_url('login'));
+		}
 	}
 
 	function index(){
@@ -33,29 +33,72 @@ class Lelang extends CI_Controller
 	}
 
 	function do_add(){
-		$this->form_validation->set_rules('nama', 'Nama', 'trim|required');
+		/*$this->form_validation->set_rules('nama', 'Nama', 'trim|required');
 		$this->form_validation->set_rules('username', 'Username', 'trim|required');
 		$this->form_validation->set_rules('email', 'Email', 'trim|required');
-		$this->form_validation->set_rules('password', 'Password', 'trim|required');
-		if ($this->form_validation->run() == FALSE) {
-			redirect('register');
-		} else {
-			$nama = $this->input->post('nama');
-			$username = $this->input->post('username');
-			$email = $this->input->post('email');
-			$password = $this->input->post('password');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');*/
 
-			$data = array(
-				'nama' => $nama,
-				'username' => $username,
-				'email' => $email,
-				'password' => $password,
-				'role' => 0,
-				'is_ban' => 0
-			);
+		/*$nama = $this->input->post('nama');
+		$username = $this->input->post('username');
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
 
-			$this->m_login->input_data($data,'user');
-			redirect('login');
-		}    
+		$data = array(
+			'nama' => $nama,
+			'username' => $username,
+			'email' => $email,
+			'password' => $password,
+			'role' => 0,
+			'is_ban' => 0
+		);
+
+		$this->m_login->input_data($data,'user');
+		redirect('login');*/
+
+		$judul = $this->input->post('judul');
+		$deskripsi = $this->input->post('deskripsi');
+		$foto = $this->input->post('foto');
+		$kondisi = $this->input->post('kondisi');
+		$final_bid = $this->input->post('final_bid');
+		$next_bid = $this->input->post('next_bid');
+		$kategori = $this->input->post('kategori');
+		$tanggal = $this->input->post('tanggal');
+		$id_pelelang = $this->session->userdata('id_user');
+
+		$now = new DateTime();
+		//echo $now->format('Y-m-d');
+		$status = ($now->format('Y-m-d')>$tanggal) ? 1 : 0 ;
+
+		$data = array(
+			'id_lelang'=> null,
+			'judul' => $judul,
+			'deskripsi' => $deskripsi,
+			'foto' => $foto,
+			'kondisi' => $kondisi,
+			'status' => $status,
+			'next_bid' => $next_bid,
+			'final_bid' => $final_bid,
+			'total_bidder' => 0,
+			'kategori' => $kategori,
+			'id_pemenang' => 0,
+			'id_pelelang' => $id_pelelang,
+			'id_kota' => 158,
+			'id_provinsi' => 11,
+		);
+
+		$this->m_lelang->tambah_data($data);
+		redirect('lelang');
+		/*$data = array(
+			'nama' => $nama,
+			'username' => $username,
+			'email' => $email,
+			'password' => $password,
+			'role' => 0,
+			'is_ban' => 0
+		);
+
+		$this->m_login->input_data($data,'user');
+		redirect('login');*/
+
 	}
 }
--- application/views/lelang/view_add_lelang.php
+++ application/views/lelang/view_add_lelang.php
@@ -31,29 +31,29 @@
             <div class="card">
                 <h5 class="card-header">Buat Lelang</h5>
                 <div class="card-body">
-                    <form action="<?php echo base_url('lelang/add');?>" method="post">
+                    <form action="<?php echo base_url('lelang/do_add');?>" method="post">
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="inputGroup-sizing-default">
-                                    <i class="fas fa-edit"></i>
+                                    <i class="fas fa-edit"></i>&nbsp;Judul
                                 </span>
                             </div>
-                            <input type="text" name="judul" title="judul" class="form-control login-input" placeholder="judul" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                            <input type="text" name="judul" title="judul" class="form-control login-input" placeholder="Judul iklan" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
                         </div>
 
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="inputGroup-sizing-default">
-                                    <i class="fas fa-edit"></i>
+                                    <i class="fas fa-edit"></i>&nbsp;Deskripsi
                                 </span>
                             </div>
-                            <input type="text" name="deskripsi" title="deskripsi" class="form-control login-input" placeholder="deskripsi" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                            <input type="text" name="deskripsi" title="deskripsi" class="form-control login-input" placeholder="Deskripsi iklan" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
                         </div>
                         
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="inputGroup-sizing-default">
-                                    <i class="fas fa-images"></i>
+                                    <i class="fas fa-images"></i>&nbsp;Gambar
                                 </span>
                             </div>
                             <input type="text" name="foto" title="foto" class="form-control login-input" placeholder="foto" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
@@ -62,37 +62,57 @@
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="inputGroup-sizing-default">
-                                    <i class="fas fa-search"></i>
+                                    <i class="fas fa-search"></i>&nbsp;Kondisi
                                 </span>
                             </div>
-                            <input type="text" name="kondisi" title="kondisi" class="form-control login-input" placeholder="kondisi" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                            <select class="custom-select" id="kondisi" name="kondisi">
+                                <option value="1" selected>Baru</option>
+                                <option value="2">Bekas</option>
+                            </select>
+                            <!--<input type="text" name="kondisi" title="kondisi" class="form-control login-input" placeholder="kondisi" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>-->
                         </div>
                         
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="inputGroup-sizing-default">
-                                    <i class="fas fa-gavel"></i>
+                                    <i class="fas fa-gavel"></i>&nbsp;Bid Awal
                                 </span>
                             </div>
-                            <input type="text" name="next_bid" title="next_bid" class="form-control login-input" placeholder="Next Bid" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                            <input type="number" name="final_bid" title="final_bid" class="form-control login-input" placeholder="Nominal Next Bid" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-default">
+                                    <i class="fas fa-gavel"></i>&nbsp;Next Bid
+                                </span>
+                            </div>
+                            <input type="number" name="next_bid" title="next_bid" class="form-control login-input" placeholder="Nominal Next Bid" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
                         </div>
                         
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="inputGroup-sizing-default">
-                                    <i class="fas fa-object-group"></i>
+                                    <i class="fas fa-object-group"></i>&nbsp;Kategori
                                 </span>
                             </div>
-                            <input type="text" name="kategori" title="kategori" class="form-control login-input" placeholder="kategori" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                            <select class="custom-select" id="kategori" name="kategori">
+                                <option value="1" selected>Mobil</option>
+                                <option value="2">Motor</option>
+                                <option value="3">Properti</option>
+                                <option value="4">Gadget</option>
+                                <option value="5">Hobi</option>
+                            </select>
+                            <!--<input type="text" name="kategori" title="kategori" class="form-control login-input" placeholder="kategori" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>-->
                         </div>
                         
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="inputGroup-sizing-default">
-                                    <i class="far fa-calendar"></i>
+                                    <i class="far fa-calendar"></i>&nbsp;Iklan Berakhir
                                 </span>
                             </div>
-                            <input type="text" name="tanggal" title="tanggal" class="form-control login-input" placeholder="tanggal" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                            <input type="date" name="tanggal" title="tanggal" class="form-control login-input" placeholder="tanggal" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
                         </div>
                         
 
